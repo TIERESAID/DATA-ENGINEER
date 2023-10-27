a238fcada734a67abec841c22408d30606ecf473
TIERESAID <vendeurn1@gmail.com>
2023-10-27 16:51:17 +0000

    4.2.1, 4.2.2

--- 4. Подготовка данных витрины/2. Работа с дублями/Задание 1/realization.sql
+++ 4. Подготовка данных витрины/2. Работа с дублями/Задание 1/realization.sql
@@ -1,1 +1,9 @@
 -- добавьте код сюда
+-- Write a query on the user_contacts table that returns two fields: client_id and phone. Only one record with the maximum creation date created_at should be left for each client.
+SELECT client_id, phone
+FROM (
+  SELECT client_id, phone, created_at,
+         ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY created_at DESC) AS row_num
+  FROM user_contacts
+) AS subquery
+WHERE row_num = 1;
--- 4. Подготовка данных витрины/2. Работа с дублями/Задание 2/realization.sql
+++ 4. Подготовка данных витрины/2. Работа с дублями/Задание 2/realization.sql
@@ -1,1 +1,5 @@
 -- добавьте код сюда
+-- Write a query on the user_contacts table that returns two fields: client_id and phone. Only one record with the maximum creation date created_at should be left for each client.
+SELECT DISTINCT ON (client_id) client_id, phone
+FROM user_contacts
+ORDER BY client_id, created_at DESC;

df2a053474378372f1022de37e0323712fdc9da8
TIERESAID <vendeurn1@gmail.com>
2023-10-27 11:39:24 +0000

    4.1, 4.2

--- 4. Подготовка данных витрины/1. Качество данных/Задание 1/realization.sql
+++ 4. Подготовка данных витрины/1. Качество данных/Задание 1/realization.sql
@@ -1,1 +1,8 @@
 -- добавьте код сюда
+-- Write a query on the user_attributes table to check for duplicates on the client_id field. The query should return two fields - total (number of records) and uniq (number of unique values):
+SELECT
+    COUNT(*) AS total,
+    COUNT(DISTINCT  client_id) AS uniq
+FROM
+    user_attributes;
+
--- 4. Подготовка данных витрины/1. Качество данных/Задание 2/realization.sql
+++ 4. Подготовка данных витрины/1. Качество данных/Задание 2/realization.sql
@@ -1,1 +1,4 @@
 -- добавьте код сюда
+-- Replace the omissions in the payment_amount field with 0 without changing the rest of the data.
+SELECT COALESCE(payment_amount, 0) AS payment_amount
+FROM user_payment_log;

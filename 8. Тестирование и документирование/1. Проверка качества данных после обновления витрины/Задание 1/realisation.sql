5e344c10caefbdbed0b5a0e033f2cc056a656bf7
TIERESAID <vendeurn1@gmail.com>
2023-10-31 14:17:07 +0000

    8 exo

--- 8. Тестирование и документирование/1. Проверка качества данных после обновления витрины/Задание 1/realisation.sql
+++ 8. Тестирование и документирование/1. Проверка качества данных после обновления витрины/Задание 1/realisation.sql
@@ -1,3 +1,9 @@
+/*
+You are given the table task3_record_log in the lesson47 schema. The table is a log of records. There are two fields in the table - the identifier of the row in the log row_id and the date of record generation row_dttm.
+It is inconvenient for analysts to use the table because the field with the date row_dttm contains a string data type.
+The manager sets you the task to replace the row_dttm field type with timestamp indicating the Moscow time zone. You need to replace the data type while preserving the existing values.
+To check the result, execute the SQL query below and copy the value in the row_dttm field after conversion and paste it into the form.
+*/
 -- Alter the data type of row_dttm to TIMESTAMP with time zone for Europe/Moscow time zone
 ALTER TABLE lesson47.task3_record_log
 ALTER COLUMN row_dttm TYPE TIMESTAMP WITH TIME ZONE;
--- 8. Тестирование и документирование/1. Проверка качества данных после обновления витрины/Задание 2/realisation.sql
+++ 8. Тестирование и документирование/1. Проверка качества данных после обновления витрины/Задание 2/realisation.sql
@@ -1,3 +1,11 @@
+/*
+You are given the table task4_user_events in the lesson47 schema. The table is a log of user actions. There are three fields in the table:
+event_dttm - date and time of the event.
+event_type - event type: visit, login, payment.
+user_id - user ID.
+There is no primary key in the table. You need to add the id column to the table and make it the primary key.
+*/
+
 -- Add the id column with a serial data type
 ALTER TABLE lesson47.task4_user_events
 ADD COLUMN id SERIAL;

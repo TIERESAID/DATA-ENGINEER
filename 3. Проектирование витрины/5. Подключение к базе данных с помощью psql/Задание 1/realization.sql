d8bbaebf1a39e6b085bc0bb537b34c2995efcb78
TIERESAID <vendeurn1@gmail.com>
2023-11-01 11:01:09 +0000

    correct 3

--- 3. Проектирование витрины/5. Подключение к базе данных с помощью psql/Задание 1/realization.sql
+++ 3. Проектирование витрины/5. Подключение к базе данных с помощью psql/Задание 1/realization.sql
@@ -4,3 +4,8 @@
 SELECT count(*)
 FROM information_schema.tables
 WHERE table_schema = 'public' AND table_type = 'BASE TABLE';
+
+--if you need more information about the structure and properties of the "main2" index or want to know which columns it covers, you can use the following query
+SELECT indexname, indexdef
+FROM pg_indexes
+WHERE tablename = 'user_activity_log' AND indexname = 'main2';

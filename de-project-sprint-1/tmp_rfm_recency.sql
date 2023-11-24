b9a2d82c62f95dcbaeee9fbe7bc4fe57346f60c5
Tiere <58536528+TIERESAID@users.noreply.github.com>
2023-11-24 07:55:08 +0100

    Update tmp_rfm_recency.sql

--- de-project-sprint-1/tmp_rfm_recency.sql
+++ de-project-sprint-1/tmp_rfm_recency.sql
@@ -1,1 +1,9 @@
-
+-- Calculate Recency for each customer
+SELECT
+    user_id,
+    ntile(5) over(partition by (extract(day from (max(order_ts) - min(order_ts))))) as recency
+FROM
+    production.orders
+where status = 4 and order_ts >= '01.01.2022'::timestamp
+group by user_id 
+order by user_id ;

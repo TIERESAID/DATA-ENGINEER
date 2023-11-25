695815ae76e226ce3cbf7312091b969be52a2468
Tiere <58536528+TIERESAID@users.noreply.github.com>
2023-11-25 06:02:15 +0100

    Update tmp_rfm_monetary_value.sql

--- de-project-sprint-1/tmp_rfm_monetary_value.sql
+++ de-project-sprint-1/tmp_rfm_monetary_value.sql
@@ -1,1 +1,12 @@
-
+SELECT
+    user_id,
+    sum(o."payment"),
+    NTILE(5) OVER (ORDER BY sum(o."payment")) AS "monetary"
+FROM
+    analysis.orders o
+WHERE
+    o.status = 4 
+GROUP BY
+    user_id
+ORDER BY
+    user_id;
